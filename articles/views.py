690808498046f14a16a7190cafcf9d2212f80ebc
bellobambo <bellobambo21@gmail.com>
2023-06-28 19:58:21 +0100

    login/logout

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -9,4 +10,12 @@ def article_list(request):
 
 def article_details(request , slug):
     return HttpResponse(slug)
+    # return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
+
+
 
